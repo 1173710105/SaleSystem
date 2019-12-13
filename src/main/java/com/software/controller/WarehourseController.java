976f931da6aa113863d5d62d29db687244317c81
1173710209 <47965226+1173710209@users.noreply.github.com>
2019-12-13 14:29:12 +0800

    更新了WareHourseController

--- src/main/java/com/software/controller/WarehourseController.java
+++ src/main/java/com/software/controller/WarehourseController.java
@@ -1,43 +1,42 @@
 package com.software.controller;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.software.domain.Staff;
+import com.software.domain.Warehourse;
 
 @RestController
-@RequestMapping("/staff")
-public class StaffController {
+@RequestMapping("/warehourse")
+public class WarehourseController {
 	@RequestMapping("/queryById")
-	public Staff queryStaffById(@RequestBody Staff param){
-		Staff result = null;
-		
+	public Warehourse queryWarehourseById(@RequestBody Warehourse param){
+		Warehourse result = null;
 		
 		return result;
 	}
 	
 	@RequestMapping("/query")
-	public List<Staff> queryStaff(@RequestBody Staff pamram){
-		List<Staff> result = null;
+	public List<Warehourse> queryWarehourse(@RequestBody Warehourse pamram){
+		List<Warehourse> result = null;
 		return result;
 	}
 	
 	@RequestMapping("/add")
-	public String addStaff(@RequestBody Staff param){
+	public String addWarehourse(@RequestBody Warehourse param){
 		return "success";
 	}
 	
 	@RequestMapping("/delete")
-	public String deleteStaff(@RequestBody Staff param){
+	public String deleteWarehourse(@RequestBody Warehourse param){
 		return "success";
 	}
 	
 	@RequestMapping("/update")
-	public String updateStaff(@RequestBody Staff param){
+	public String updateWarehourse(@RequestBody Warehourse param){
 		return "success";
 	}
 }
