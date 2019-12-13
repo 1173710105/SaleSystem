5120aaf04d525ad011f915e0b89ce23f00632d31
1173710209 <47965226+1173710209@users.noreply.github.com>
2019-12-13 12:56:06 +0800

    debug

--- src/main/java/com/software/controller/ClientController.java
+++ src/main/java/com/software/controller/ClientController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.software.topservice.ClientManagerService;
+import com.software.trans.ReceiveClient;
 
 
 @RestController
