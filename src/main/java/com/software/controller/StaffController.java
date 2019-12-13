8cf40ad54e97aadfc898974b2e021a9574111123
1173710209 <47965226+1173710209@users.noreply.github.com>
2019-12-13 14:51:38 +0800

    更新了StaffController

--- src/main/java/com/software/controller/StaffController.java
+++ src/main/java/com/software/controller/StaffController.java
@@ -3,6 +3,7 @@ package com.software.controller;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,32 +13,134 @@ import com.software.domain.Staff;
 @RestController
 @RequestMapping("/staff")
 public class StaffController {
+	@Autowired
+	
 	@RequestMapping("/queryById")
-	public Staff queryStaffById(@RequestBody Staff param){
-		Staff result = null;
+	public Staff queryStaffById(@RequestBody Map<String, String> param){
+		String tablename = param.get("tablename");
+		String id = param.get("id");
+		String password = param.get("password");
+		Integer hourseid = Integer.valueOf(param.get("hourseid"));
+		String name = param.get("name");
+		String gender = param.get("gender");
+		String phone = param.get("phone");
+		String email = param.get("email");
 		
+		Staff staff = new Staff();
+		staff.setTablename(tablename);
+		staff.setId(id);
+		staff.setPassword(password);
+		staff.setHourseid(hourseid);
+		staff.setName(name);
+		staff.setGender(gender);
+		staff.setPhone(phone);
+		staff.setEmail(email);
+		staff.setLabel("valid");
+		
+		
+		Staff result = null;
 		
 		return result;
 	}
 	
 	@RequestMapping("/query")
-	public List<Staff> queryStaff(@RequestBody Staff pamram){
+	public List<Staff> queryStaff(@RequestBody Map<String, String> param){
+		String tablename = param.get("tablename");
+		String id = param.get("id");
+		String password = param.get("password");
+		Integer hourseid = Integer.valueOf(param.get("hourseid"));
+		String name = param.get("name");
+		String gender = param.get("gender");
+		String phone = param.get("phone");
+		String email = param.get("email");
+		
+		Staff staff = new Staff();
+		staff.setTablename(tablename);
+		staff.setId(id);
+		staff.setPassword(password);
+		staff.setHourseid(hourseid);
+		staff.setName(name);
+		staff.setGender(gender);
+		staff.setPhone(phone);
+		staff.setEmail(email);
+		staff.setLabel("valid");
+		
 		List<Staff> result = null;
 		return result;
 	}
 	
 	@RequestMapping("/add")
-	public String addStaff(@RequestBody Staff param){
+	public String addStaff(@RequestBody Map<String, String> param){
+		String tablename = param.get("tablename");
+		String id = param.get("id");
+		String password = param.get("password");
+		Integer hourseid = Integer.valueOf(param.get("hourseid"));
+		String name = param.get("name");
+		String gender = param.get("gender");
+		String phone = param.get("phone");
+		String email = param.get("email");
+		
+		Staff staff = new Staff();
+		staff.setTablename(tablename);
+		staff.setId(id);
+		staff.setPassword(password);
+		staff.setHourseid(hourseid);
+		staff.setName(name);
+		staff.setGender(gender);
+		staff.setPhone(phone);
+		staff.setEmail(email);
+		staff.setLabel("valid");
+		
 		return "success";
 	}
 	
 	@RequestMapping("/delete")
-	public String deleteStaff(@RequestBody Staff param){
+	public String deleteStaff(@RequestBody Map<String, String> param){
+		String tablename = param.get("tablename");
+		String id = param.get("id");
+		String password = param.get("password");
+		Integer hourseid = Integer.valueOf(param.get("hourseid"));
+		String name = param.get("name");
+		String gender = param.get("gender");
+		String phone = param.get("phone");
+		String email = param.get("email");
+		
+		Staff staff = new Staff();
+		staff.setTablename(tablename);
+		staff.setId(id);
+		staff.setPassword(password);
+		staff.setHourseid(hourseid);
+		staff.setName(name);
+		staff.setGender(gender);
+		staff.setPhone(phone);
+		staff.setEmail(email);
+		staff.setLabel("valid");
+		
 		return "success";
 	}
 	
 	@RequestMapping("/update")
-	public String updateStaff(@RequestBody Staff param){
+	public String updateStaff(@RequestBody Map<String, String> param){
+		String tablename = param.get("tablename");
+		String id = param.get("id");
+		String password = param.get("password");
+		Integer hourseid = Integer.valueOf(param.get("hourseid"));
+		String name = param.get("name");
+		String gender = param.get("gender");
+		String phone = param.get("phone");
+		String email = param.get("email");
+		
+		Staff staff = new Staff();
+		staff.setTablename(tablename);
+		staff.setId(id);
+		staff.setPassword(password);
+		staff.setHourseid(hourseid);
+		staff.setName(name);
+		staff.setGender(gender);
+		staff.setPhone(phone);
+		staff.setEmail(email);
+		staff.setLabel("valid");
+		
 		return "success";
 	}
 }
