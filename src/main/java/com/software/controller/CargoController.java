8d34a834ea6486cfa94a768e5cbf4c31ec219d31
1173710209 <47965226+1173710209@users.noreply.github.com>
2019-12-13 14:38:29 +0800

    1

--- src/main/java/com/software/controller/CargoController.java
+++ src/main/java/com/software/controller/CargoController.java
@@ -4,47 +4,57 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.software.topservice.ItemManagerSerivce;
 import com.software.trans.ReceiveCargo;
 
 @RestController
 @RequestMapping("/cargo")
 public class CargoController {
 	
+	@Autowired
+	private ItemManagerSerivce service;
 	@RequestMapping("/queryById")
 	public ReceiveCargo queryCargoById(@RequestBody ReceiveCargo param){
-		
-		ReceiveCargo result = new ReceiveCargo();
+		param.setLabel("valid");
+		ReceiveCargo result = service.selectByPrimaryKey(param);
 		return result;
 	}
 	
 	@RequestMapping("/query")
 	public List<ReceiveCargo> queryCargo(@RequestBody ReceiveCargo param){
-		List<ReceiveCargo> result = new ArrayList<ReceiveCargo>();
-		
+		param.setLabel("valid");
+		List<ReceiveCargo> result = service.select(param);
 		return result;
 	}
 	
 	@RequestMapping("/add")
 	public String addCargo(@RequestBody ReceiveCargo param){
-		
-		
+		param.setLabel("valid");
+		service.insertSelective(param);
 		return "success";
 	}
 	
 	@RequestMapping("/delete")
 	public String deleteCargo(@RequestBody ReceiveCargo param){
-		
+		param.setLabel("invalid");
+		service.updateByPrimaryKeySelective(param);
 		return "success";
 	}
 	
 	@RequestMapping("/update")
 	public String updateCargo(@RequestBody ReceiveCargo param){
-		
-		
+		param.setLabel("valid");
+		service.updateByPrimaryKeySelective(param);
 		return "success";
+	}
+	
+	public Map<Integer, String> typeMenu()
+	{
+		return service.typeMenu();
 	}
 }
