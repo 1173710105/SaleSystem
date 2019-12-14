207426616eb39b7eb4b395b3ead476a0a6be8fce
1173710209 <47965226+1173710209@users.noreply.github.com>
2019-12-14 12:18:54 +0800

    Update ShopmanagerController.java

--- src/main/java/com/software/controller/ShopmanagerController.java
+++ src/main/java/com/software/controller/ShopmanagerController.java
@@ -1,5 +1,9 @@
 package com.software.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +14,16 @@ import com.software.domain.StoreManager;
 @RequestMapping("/shopmanager")
 public class ShopmanagerController {
 //	@RequestMapping("/queryById")
-//	public StoreManager queryManagerById(@RequestBody ){
+//	public StoreManager queryManagerById(@RequestBody Map<String, String>){
+//		StoreManager storeManager = new
+//		
+//		StoreManager result = new StoreManager();
+//		return result;
+//	}
+//	
+//	public List<StoreManager> queryManager(@RequestBody Map<String, String>){
 //		
 //	}
+//	
+//	
 }
