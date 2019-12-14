0a4ac9ba4ebf50176d7783ab96b42804093ad7aa
1173710209 <47965226+1173710209@users.noreply.github.com>
2019-12-14 12:33:52 +0800

    事件

--- src/main/java/com/software/controller/WarehourseOrderController.java
+++ src/main/java/com/software/controller/WarehourseOrderController.java
@@ -2,53 +2,76 @@ package com.software.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.software.topservice.WarehourseOrderManagerService;
 import com.software.trans.ReceiveWarehourseOrder;
 import com.software.trans.SendWarehourseOrder;
 
 @RestController
 @RequestMapping("/warehourseOrder")
-public class WarehourseOrderController {
+public class WarehourseOrderController 
+{
+	@Autowired
+	private WarehourseOrderManagerService service;
+	
 	@RequestMapping("/queryById")
 	public SendWarehourseOrder queryWarehourseOrderById(@RequestBody ReceiveWarehourseOrder param){
 		
-		SendWarehourseOrder result = null;
+		SendWarehourseOrder result = service.select(param).get(0);
 		return result;
 	}
 	
 	@RequestMapping("/query")
 	public List<SendWarehourseOrder> queryWarehourseOrder(@RequestBody ReceiveWarehourseOrder param){
-		List<SendWarehourseOrder> result = null;
+		List<SendWarehourseOrder> result = service.select(param);
 		return result;
 	}
 	
 	@RequestMapping("/insert")
 	public String insertWarehourseOrder(@RequestBody List<ReceiveWarehourseOrder> param){
+		if (param.size()==0) 
+		{
+			return "";
+		}
+		param.get(0).setStatus(1+"");
+		service.insert(param);
 		return "success";
 	}
 	
 	@RequestMapping("/update")
 	public String updateWarehourseOrder(@RequestBody List<ReceiveWarehourseOrder> param){
+		if (param.size()==0) 
+		{
+			return "";
+		}
+		param.get(0).setStatus(1+"");
+		service.update(param);
 		return "success";
 	}
 	
 	@RequestMapping("/delete")
 	public String deleteWarehourseOrder(@RequestBody ReceiveWarehourseOrder param){
+		service.delete(param);
 		return "success";
 	}
 	
 	@RequestMapping("/apply")
 	public String applyWarehourseOrder(@RequestBody ReceiveWarehourseOrder param){
 		// update
+		param.setStatus(2+"");
+		
 		return "success";
 	}
 	
 	@RequestMapping("/pass")
 	public String passWarehourseOrder(@RequestBody ReceiveWarehourseOrder param){
 		// check
+		param.setStatus(4+"");
+		String result = service.checkOrder(param);
 		return "success";
 	}
 }
