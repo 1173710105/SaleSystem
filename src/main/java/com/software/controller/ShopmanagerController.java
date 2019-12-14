c0f0b5d9ddaad2b33c96c3c1d7f705fcb8a45ffa
1173710209 <47965226+1173710209@users.noreply.github.com>
2019-12-14 13:18:43 +0800

    更新店长店员Controller

--- src/main/java/com/software/controller/ShopmanagerController.java
+++ src/main/java/com/software/controller/ShopmanagerController.java
@@ -9,21 +9,81 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.software.domain.StoreManager;
+import com.software.domain.StoreManager;
 
 @RestController
 @RequestMapping("/shopmanager")
 public class ShopmanagerController {
-//	@RequestMapping("/queryById")
-//	public StoreManager queryManagerById(@RequestBody Map<String, String>){
-//		StoreManager storeManager = new
-//		
-//		StoreManager result = new StoreManager();
-//		return result;
-//	}
-//	
-//	public List<StoreManager> queryManager(@RequestBody Map<String, String>){
-//		
-//	}
-//	
-//	
+	@RequestMapping("/queryById")
+	public StoreManager queryManagerById(@RequestBody Map<String, String> param){
+		String id = param.get("id");
+		String password = param.get("password");
+		String name = param.get("name");
+		String gender = param.get("gender");
+		String phone = param.get("phone");
+		String email = param.get("email");
+		String hourseid = param.get("hourseid");
+		
+		StoreManager storemanager = new StoreManager();
+		storemanager.setId(id);
+		storemanager.setPassword(password);
+		storemanager.setName(name);
+		storemanager.setGender(gender);
+		storemanager.setPhone(phone);
+		storemanager.setEmail(email);
+		storemanager.setLabel("valid");
+		
+		
+		StoreManager result = null;
+		return result;
+	}
+	
+	@RequestMapping("/query")
+	public List<StoreManager> queryManager(@RequestBody Map<String, String> param){
+		String id = param.get("id");
+		String password = param.get("password");
+		String name = param.get("name");
+		String gender = param.get("gender");
+		String phone = param.get("phone");
+		String email = param.get("email");
+		String hourseid = param.get("hourseid");
+		
+		StoreManager storemanager = new StoreManager();
+		storemanager.setId(id);
+		storemanager.setPassword(password);
+		storemanager.setName(name);
+		storemanager.setGender(gender);
+		storemanager.setPhone(phone);
+		storemanager.setEmail(email);
+		storemanager.setLabel("valid");
+		
+		List<StoreManager> result = null;
+		return result;
+	}
+	
+	@RequestMapping("/insert")
+	public String insertManager(@RequestBody Map<String, String> param){
+		String id = param.get("id");
+		String password = param.get("password");
+		String name = param.get("name");
+		String gender = param.get("gender");
+		String phone = param.get("phone");
+		String email = param.get("email");
+		String hourseid = param.get("hourseid");
+		
+		StoreManager storemanager = new StoreManager();
+		storemanager.setId(id);
+		storemanager.setPassword(password);
+		storemanager.setName(name);
+		storemanager.setGender(gender);
+		storemanager.setPhone(phone);
+		storemanager.setEmail(email);
+		storemanager.setLabel("valid");
+		
+		
+		
+		return "success";
+	}
+	
+	
 }
