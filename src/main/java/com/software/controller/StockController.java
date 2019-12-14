8172ef4888c37694fc5f9cc0631ef35464f51f27
1173710209 <47965226+1173710209@users.noreply.github.com>
2019-12-14 11:03:42 +0800

    更新了stock类

--- src/main/java/com/software/controller/StockController.java
+++ src/main/java/com/software/controller/StockController.java
@@ -1,5 +1,6 @@
 package com.software.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,9 +8,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.software.domain.GeneralManager;
+import com.software.trans.Stock;
 
 @RestController
 @RequestMapping("/stock")
 public class StockController {
+	@RequestMapping("/queryById")
+	public List<Stock> queryStockByWarehourseId(@RequestBody Stock param){
+		List<Stock> result = null;
+		return result;
+	}
+	
 	
 }
