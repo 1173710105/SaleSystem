2d5995554739d630ce84a677365b555fcac38c12
jjd <jjdjiang@qq.com>
2019-12-14 18:49:28 +0800

    修改homeindexjs

--- src/main/webapp/js/saleSys/homeindex.js
+++ src/main/webapp/js/saleSys/homeindex.js
@@ -1,13 +1,13 @@
 
 window.onload = function() {
     //TODO 添加职位过滤
-    var position = $('#type').val();
-    if(postion != getCookie("position")) {
-        alert("");
-        window.location.href = "../login.html";
-        return;
-    }
-    // if (getCookie("sessionid") == "") {
+    // var position = $('#type').val();
+    // if(postion != getCookie("position")) {
+    //     alert("验证失败，请重新登录");
+    //     window.location.href = "../login.html";
+    //     return;
+    // }
+    // if (getCookie("id") == "") {
     // //登录
     // alert("未登录,请登录后操作");
     // window.location.href = "../login.html";
