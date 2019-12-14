648bb62a6917d385b0f2f6a4cdab1a92a5e8c203
jjd <jjdjiang@qq.com>
2019-12-14 11:03:26 +0800

    update homeindex.js

--- src/main/webapp/js/saleSys/homeindex.js
+++ src/main/webapp/js/saleSys/homeindex.js
@@ -1,6 +1,12 @@
 
 window.onload = function() {
     //TODO 添加职位过滤
+    var position = $('#type').val();
+    if(postion != getCookie("position")) {
+        alert("");
+        window.location.href = "../login.html";
+        return;
+    }
     // if (getCookie("sessionid") == "") {
     // //登录
     // alert("未登录,请登录后操作");
