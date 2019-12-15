22abfe19d24f5da8930fc2385d73f90b600ae691
jjd <jjdjiang@qq.com>
2019-12-15 14:55:59 +0800

    添加pos清空

--- src/main/webapp/js/saleSys/assistant_pos.js
+++ src/main/webapp/js/saleSys/assistant_pos.js
@@ -140,3 +140,23 @@ $('#submit-btn').click(function() {
     document.getElementById('cargo-num').value = "";
     document.getElementById('temp-cargo-list').innerHTML = "";
 });
+
+$('#clear-btn').click(function() {
+    //清空内容
+    document.getElementById('client-id').value = "";
+    document.getElementById('client-name').value = "";
+    document.getElementById('cargo-id').value = "";
+    document.getElementById('cargo-name').value = "";
+    document.getElementById('cargo-retail-price').value = "";
+    document.getElementById('cargo-num').value = "";
+    document.getElementById('temp-cargo-list').innerHTML = "";
+
+    $('#total-price').val("");
+    $("#actual-recive").val("");
+    $('#change').val("");
+
+    document.getElementById("temp-cargo-list").innerHTML = "";
+
+    cargoList = [];
+    cargoNum = [];
+});
