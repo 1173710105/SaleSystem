cbde4ea987e9aafc051ca80406a7cbb0aa22d7a4
jjd <jjdjiang@qq.com>
2019-12-15 14:56:43 +0800

    11

--- src/main/webapp/js/saleSys/assistant_pos.js
+++ src/main/webapp/js/saleSys/assistant_pos.js
@@ -1,10 +1,10 @@
 //POS机操作
 window.onload = function () {
-    if (getCookie("sessionid") == "") {
-    //登录
-    alert("未登录,请登录后操作");
-    window.location.href = "../login.html";
-    return;
+    if (getCookie("id") == "") {
+        //登录
+        alert("未登录,请登录后操作");
+        window.location.href = "../login.html";
+        return;
     }
 }
 
