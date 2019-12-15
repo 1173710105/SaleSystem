e79dc9d5c959a303bbd6c8e9a47cd3fc61db4c43
1173710105 <1249614654@qq.com>
2019-12-15 14:20:37 +0800

    更新了saleorder订单处理仓库库存的细节

--- src/main/java/com/software/topservice/SaleOrderManagerServiceImp.java
+++ src/main/java/com/software/topservice/SaleOrderManagerServiceImp.java
@@ -13,7 +13,6 @@ import com.software.domain.ItemToPrice;
 import com.software.domain.SaleorderCommon;
 import com.software.domain.SaleorderItem;
 import com.software.domain.WarehourseDetail;
-import com.software.service.ItemService;
 import com.software.service.ItemToPriceService;
 import com.software.service.SaleorderCommonService;
 import com.software.service.SaleorderItemService;
@@ -67,6 +66,7 @@ public class SaleOrderManagerServiceImp implements SaleOrderManagerService
 		// 插入的时候是没有ID的，插入，查出来， 这样可以后去ID
 		ReceiveOrder example =  orderList.get(0);
 		example.fillTablename();
+		
 		SaleorderCommon exampleCommon = example.toCommon();
 		exampleCommon.setId(null);
 		exampleCommon.setException(String.valueOf(getRandom()));
@@ -75,10 +75,19 @@ public class SaleOrderManagerServiceImp implements SaleOrderManagerService
 		// 插入商品, 最后查出价格之后还需要填上，毛利润
 		SaleorderCommon resultCommon = commonService.select(exampleCommon).get(0);
 		resultCommon.setTablename(example.getCommontablename());
-		SaleorderItem tempItem;
+		
 		// 查利润
 		ItemToPrice tempPrice = new ItemToPrice();
 		tempPrice.setTablename(example.getItemtopricetable());
+		
+		// 记录需要插入的商品
+		SaleorderItem tempItem;
+		List<SaleorderItem> itemList = new ArrayList<>();
+		
+		// 记录数量变化
+		List<WarehourseDetail> detailList = new ArrayList<WarehourseDetail>();
+		WarehourseDetail tempDetail;
+		
 		float margin = 0;
 		for (ReceiveOrder receiveOrder : orderList) 
 		{
@@ -86,14 +95,42 @@ public class SaleOrderManagerServiceImp implements SaleOrderManagerService
 			tempItem = receiveOrder.toItem();
 			tempItem.setViceid(null);
 			tempItem.setId(resultCommon.getId());
-			itemService.insertSelective(tempItem);
+			itemList.add(tempItem);
+			
+			//itemService.insertSelective(tempItem);
+			
+			// 如果是零售，一步跳到5，需要检测数目够不够
+			if (example.getStatus().equals("5")) 
+			{
+				tempDetail = isEnough(tempItem, receiveOrder.getWarehoursedetailtablename());
+				if (tempDetail==null) 
+				{
+					System.out.println("数量不知，无法零售");
+					commonService.deleteByPrimaryKey(resultCommon);  // 删除订单
+					return;
+				}
+				detailList.add(tempDetail);
+			}
 			// 算利润
 			tempPrice.setId(tempItem.getItemid());
 			margin += calMargin(priceService.selectByPrimaryKey(tempPrice), example.getType())*tempItem.getItemnum();
+			
 		}
+		
 		//更新利润
 		resultCommon.setMargin(margin);
 		commonService.updateByPrimaryKeySelective(resultCommon);
+		
+		//detailList 更新仓库信息
+		for (WarehourseDetail detail : detailList) 
+		{
+			detailService.updateByPrimaryKeySelective(detail);
+		}
+		//itemList 插入订单物品信息
+		for (SaleorderItem item : itemList) 
+		{
+			itemService.updateByPrimaryKeySelective(item);
+		}
 	}
 
 	@Override
@@ -155,26 +192,35 @@ public class SaleOrderManagerServiceImp implements SaleOrderManagerService
 		List<WarehourseDetail> detailList = new ArrayList<>();
 		WarehourseDetail tempDetail = new WarehourseDetail();
 		WarehourseDetail resultDetail;
+		
 		for (SaleorderItem saleorderItem : itemList) 
 		{
 			// 查看当前商品仓库里面一共有多少个
-			tempDetail.setTablename(order.getWarehoursedetailtablename());
-			tempDetail.setItemid(saleorderItem.getItemid());
-			resultDetail = detailService.selectByPrimaryKey(tempDetail);
+			resultDetail = isEnough(saleorderItem, order.getWarehoursedetailtablename());
 			if (resultDetail==null) 
-			{
-				System.out.println("逻辑错误，订单里的商品，仓库没有");
-				return "false";
-			}
-			if (resultDetail.getItemnum()<saleorderItem.getItemnum()) 
 			{
 				// 商品数量不足
 				return "false";
 			}
-			//更新商品数量
-			resultDetail.setTablename(order.getWarehoursedetailtablename());
-			resultDetail.setItemnum(resultDetail.getItemnum()-saleorderItem.getItemnum());
 			detailList.add(resultDetail);
+			
+//			tempDetail.setTablename(order.getWarehoursedetailtablename());
+//			tempDetail.setItemid(saleorderItem.getItemid());
+//			resultDetail = detailService.selectByPrimaryKey(tempDetail);
+//			
+//			if (resultDetail==null) 
+//			{
+//				System.out.println("逻辑错误，订单里的商品，仓库没有");
+//				return "false";
+//			}
+//			if (resultDetail.getItemnum()<saleorderItem.getItemnum()) 
+//			{
+//				
+//			}
+//			//更新商品数量
+//			resultDetail.setTablename(order.getWarehoursedetailtablename());
+//			resultDetail.setItemnum(resultDetail.getItemnum()-saleorderItem.getItemnum());
+//			detailList.add(resultDetail);
 		}
 		
 		// 更改共同信息
@@ -255,6 +301,24 @@ public class SaleOrderManagerServiceImp implements SaleOrderManagerService
 		else 
 		{
 			return (float) -1.0;
+		}
+	}
+	
+	private WarehourseDetail isEnough(SaleorderItem item, String detailTablename)
+	{
+		WarehourseDetail exampleDetail = new WarehourseDetail();
+		exampleDetail.setTablename(detailTablename);
+		exampleDetail.setItemid(item.getItemid());
+		WarehourseDetail resultDetail = detailService.selectByPrimaryKey(exampleDetail);
+		if (resultDetail.getItemnum()<item.getItemnum()) 
+		{
+			return null;
+		}
+		else
+		{
+			resultDetail.setTablename(detailTablename);
+			resultDetail.setItemnum(resultDetail.getItemnum()-item.getItemnum());
+			return resultDetail;
 		}
 	}
 }
