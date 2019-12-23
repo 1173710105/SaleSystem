0fb6c017cd1ccef794c85323c7975a4fc1a95a12
jjd <jjdjiang@qq.com>
2019-12-23 16:15:37 +0800

    【修改】调整文件结构

--- src/main/webapp/js/saleSys/hometop.js
+++ src/main/webapp/js/saleSys/hometop.js
@@ -2,19 +2,29 @@
  * 用于首页顶部处理用户名显示和退出登录功能
  */
 window.onload = function(){
-    $('#greeting').html("aaa");
-    
-    // fillTopInfo();
+    fillTopInfo();
 }
 
 function fillTopInfo() {
     //判断职位,显示不同欢迎语
     var position = getCookie("position");
     if (position == "staff") {
-        $('#greeting').html("欢迎店员" + getCookie('principalname'));
+        $('#greeting').text("欢迎店员" + getCookie('principalname'));
     } else if(position == "shop-manager") {
-        $('#greeting').html("欢迎店长" + getCookie('principalname'));
+        $('#greeting').text("欢迎店长" + getCookie('principalname'));
     } else if (position == "general-manager") {
-        $('#greeting').html("欢迎经理" + getCookie('principalname'));
+        $('#greeting').text("欢迎经理" + getCookie('principalname'));
     }
 }
+
+function logout() {
+    if(confirm("确定退出登录？")) {
+        cleanCookie();
+        top.location.href = "http://localhost:8080/exam/" + "login.html";
+    }
+}
+
+function profile() {
+    
+}
+
